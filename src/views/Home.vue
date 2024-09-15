b2304c9b37c8507f1b0bdd2a75ab6c016a28b6bf
Artúr Friedrich <artur.friedrich.harka@gmail.com>
2024-09-15 18:09:13 +0200

    setup

---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  name: "Home",
+  props: ["initData"],
+  data() {
+    return {
+
+    }
+  }
+};
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
