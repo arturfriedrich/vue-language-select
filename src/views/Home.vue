0f2fa224e6237fb11170ade94f497dcac43a44c8
Artúr Friedrich <artur.friedrich.harka@gmail.com>
2024-09-15 18:37:05 +0200

    added content

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,11 +2,6 @@
 export default {
   name: "Home",
   props: ["initData", "contents"],
-  data() {
-    return {
-
-    }
-  },
   computed: {
     content() {
       return this.contents;
@@ -16,15 +11,25 @@ export default {
 </script>
 
 <template>
-  <div class="container">
+  <div class="container py-5">
     <div class="row">
-      <div class="col">
-        <h1>{{ content.welcome }}</h1>
+      <div class="col-md-8 offset-md-2">
+        <h1 class="mb-4">{{ content.title }}</h1>
+        <span v-html="content.txt" class="mb-3 d-block"></span>
+        <p class="text-muted">{{ content.author }}</p>
       </div>
     </div>
   </div>
 </template>
 
 <style scoped>
-
+h1 {
+  font-weight: 600;
+}
+span {
+  font-size: 1.1rem;
+}
+p {
+  font-size: 0.9rem;
+}
 </style>
